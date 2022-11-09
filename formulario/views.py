2b5b47a47908ad0ec70d7ba6744e97e6fae6fb03
Luciano David Dorregaray <lucianodavid2201@gmail.com>
2022-11-09 12:48:50 -0300

    Modificacion de metodo en el formulario de GET a POST, se agrego csrf_token

--- formulario/views.py
+++ formulario/views.py
@@ -4,11 +4,14 @@ from .models import Usuario
 
 def registro(request):
     users = Usuario.objects.all()
+
     return render(request, 'registro.html', {'users': users})
 
-def success(request):    
-    name = request.GET['name']
-    lastname = request.GET['lastname']
-    email = request.GET['email']
-    # Usuario.objects.create(nombre=name, apellido=lastname, email=email)
+def success(request):
+    name = request.POST['name']
+    lastname = request.POST['lastname']
+    email = request.POST['email']
+
+    Usuario.objects.create(nombre=name, apellido=lastname, email=email)
+
     return render(request, 'success.html', {'name': name, 'lastname': lastname, 'email': email})
